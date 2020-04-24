abd914decb276cab98f476ed2c90c2f7024fc186
wangjirong <408718358@qq.com>
2020-04-24 20:00:22 +0800

    add songsupload multer

--- routes/song.js
+++ routes/song.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const Router = express.Router();
+const multer =require('multer')
 
 
-Router.post('/uploadSong',async(req,res,next)=>{
+let time = '';
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'static/songs');
+    },
+    filename: (req, file, callback) => {
+        time = Date.now() + '-';
+        callback(null, time + file.originalname);
+    }
+})
 
+const upload = multer({
+    storage: storage
+});
+Router.post('/uploadSong',upload.array('files',3),async(req,res,next)=>{
+    const files = req.files;
+    res.send("success")
 })
 
 export  default Router
